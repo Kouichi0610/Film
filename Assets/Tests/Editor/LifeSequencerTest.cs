dbe37079fde693b249f00cf4c0d7d7dd3e04bcd6
Kouichi0610 <53707533+Kouichi0610@users.noreply.github.com>
2023-03-10 02:53:49 +0900

    テスト名を修正

--- Assets/Tests/Editor/LifeSequencerTest.cs
+++ Assets/Tests/Editor/LifeSequencerTest.cs
@@ -13,14 +13,22 @@ namespace Film.Tests
     public class LifeSequencerTest
     {
         [Test, Order(0)]
-        public void LifeSequencer_初期ヒットポイントを設定()
+        public void Invincible_ダメージの影響を受けないこと()
+        {
+            var sequencer = LifeSequencers.Invincible();
+            sequencer.Damage(int.MaxValue, WorldTime.FromFloat(10));
+            Assert.That(sequencer.Living(WorldTime.FromFloat(10)), Is.True);
+        }
+
+        [Test, Order(1)]
+        public void Breakable_初期ヒットポイントを設定()
         {
             var sequencer = LifeSequencers.Breakable(100);
             Assert.That(sequencer, Is.Not.Null);
         }
 
         [Test, Order(2)]
-        public void LifeSequencer_初期に設定したヒットポイントを超えるダメージを受けると死亡扱いになること()
+        public void Breakable_ヒットポイントを超えるダメージを受けると死亡扱いになること()
         {
             var sequencer = LifeSequencers.Breakable(100);
             sequencer.Damage(90, WorldTime.FromFloat(15.0f));
@@ -30,7 +38,7 @@ namespace Film.Tests
         }
 
         [Test, Order(3)]
-        public void LifeSequencer_過去からやりなおす_未来のダメージが無かったことになっていること()
+        public void Breakable_Rewind以降のダメージが無かったことになっていること()
         {
             var sequencer = LifeSequencers.Breakable(100);
             sequencer.Damage(100, WorldTime.FromFloat(10.0f));
@@ -38,14 +46,5 @@ namespace Film.Tests
             sequencer.Rewind(WorldTime.FromFloat(9.9f));
             Assert.That(sequencer.Living(WorldTime.FromFloat(10.0f)), Is.True);
         }
-
-        [Test, Order(4)]
-        public void InvincibleLiveSequencer_ダメージの影響を受けないこと()
-        {
-            var sequencer = LifeSequencers.Invincible();
-            sequencer.Damage(int.MaxValue, WorldTime.FromFloat(10));
-            Assert.That(sequencer.Living(WorldTime.FromFloat(10)), Is.True);
-        }
-
     }
 }
